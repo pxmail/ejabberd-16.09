1141b733cb2ae18d5a9afbe70f203e6451d7615f
pangxin <pangxin@pangxindeMacBook-Pro.local>
2017-02-20 12:59:16 +0800

    fix ejabberd_auth_sql.erl

--- src/ejabberd_auth_sql.erl
+++ src/ejabberd_auth_sql.erl
@@ -208,7 +208,7 @@ try_register(User, Server, Password) ->
             case is_scrammed() of
                 true ->
                     Scram = password_to_scram(Password),
-					?DEBUG("103 Scram=~p,Password=~n", [Scram, Password}]),
+					?DEBUG("103 Scram=~p,Password=~n", [Scram, Password]),
                     case catch sql_queries:add_user_scram(
                                  LServer,
                                  LUser,
