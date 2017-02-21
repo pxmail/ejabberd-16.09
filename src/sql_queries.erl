8ce991b52f6a88c0d79a9b5c929d1ff69f6ae7c7
pangxin <pangxin@pangxindeMacBook-Pro.local>
2017-02-21 17:34:27 +0800

    Fix;

--- src/sql_queries.erl
+++ src/sql_queries.erl
@@ -489,7 +489,7 @@ roster_subscribe(LServer, {LUser, SJID, Nick, SSub, SAsk, _AskMessage}) ->
 	      	{selected, []} ->
 			  	1;
 	      	{selected, [{MaxRosterId}]} ->  %%MaxRosterId=[{<<"4603">>}]
-			   ?DEBUG("1020 MaxRosterId=~p~n", [binary_to_integer((MaxRosterId)]),
+			   ?DEBUG("1020 MaxRosterId=~p~n", [binary_to_integer(MaxRosterId)]),
 		  	   binary_to_integer(MaxRosterId) + 1;
 	      	_ -> 
 	            error
